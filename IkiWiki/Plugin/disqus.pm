db423fe30db4007365fc49312a52a370e0ebfdec
Randy Smith <perstalker@vuser.org>
2012-03-24 22:07:45 -0600

    Add SEE ALSO section.

--- IkiWiki/Plugin/disqus.pm
+++ IkiWiki/Plugin/disqus.pm
@@ -134,3 +134,13 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+=head1 SEE ALSO
+
+=over 4
+
+=item http://ikiwiki.info/
+
+=item http://docs.disqus.com/developers/universal/
+
+=back
